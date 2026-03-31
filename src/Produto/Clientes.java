1369eb4bc35e95ddc4c7443e1d8fb4551c3fd013
Alisson G Costa <alisson.costa84200@gmail.com>
2026-03-31 20:38:45 -0300

    iniciando o menu 2

---
+++ src/Produto/Clientes.java
@@ -0,0 +1,17 @@
+package Produto;
+
+public class Clientes {
+    private String nomeCliente;
+    private String cpf;
+    private int numeroTelefone;
+    private int id;
+
+    public Clientes(String nomeCliente, String cpf, int numeroTelefone, int id) {
+        this.nomeCliente = nomeCliente;
+        this.cpf = cpf;
+        this.numeroTelefone = numeroTelefone;
+        this.id = id;
+    }
+}
+
+
